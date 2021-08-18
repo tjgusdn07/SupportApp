ee111933a046f468c5333c7cea5e5b86b6f0183c
진범진 <jinbeomjin@google.com>
2021-08-18 20:02:15 +0900

    8/18 // 프로필완료

--- app/src/main/java/com/example/myapplication/BoardwriteActivity.java
+++ app/src/main/java/com/example/myapplication/BoardwriteActivity.java
@@ -51,7 +51,8 @@ public class BoardwriteActivity extends AppCompatActivity {
     private FirebaseStorage storage;
     private FirebaseDatabase database;
     private String imagePath;
-
+    private ScrollView scroll;
+    private BitmapDrawable bitmap;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -112,7 +113,7 @@ public class BoardwriteActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (GET_GALLARY == requestCode && requestCode == RESULT_OK) {
+        if (GET_GALLARY == requestCode) {
             imagePath = getPath(data.getData());
             File file =new File(imagePath);
             imageView.setImageURI(Uri.fromFile(file));
